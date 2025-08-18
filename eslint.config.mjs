c36035d45e2423430c2c7c77c79c78acab04c6ee
crystal <crystal1229d@naver.com>
2025-08-18 15:42:23 +0900

    [Chore] 프로젝트 생성 및 초기 설정 - Next.js 프로젝트 생성 - ESLint, Prettier 세팅 - 불필요한 코드 정리

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,90 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from 'typescript-eslint';
+import { fixupConfigRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
 
-export default eslintConfig;
+  ...fixupConfigRules(compat.extends('next/core-web-vitals')),
+  ...fixupConfigRules(compat.extends('airbnb')),
+  ...fixupConfigRules(compat.extends('airbnb-typescript')),
+  ...fixupConfigRules(compat.extends('airbnb/hooks')),
+
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/prop-types': 'off',
+      'import/extensions': 'off',
+      'import/prefer-default-export': 'off',
+      'react/button-has-type': 'off',
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: false,
+          optionalDependencies: false,
+          peerDependencies: false,
+        },
+      ],
+      '@typescript-eslint/lines-between-class-members': 'off',
+      '@typescript-eslint/no-throw-literal': 'off',
+    },
+  },
+
+  {
+    files: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', '**/*.spec.tsx', 'jest.setup.ts'],
+    rules: {
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: true,
+          optionalDependencies: false,
+          peerDependencies: false,
+        },
+      ],
+    },
+  },
+
+  eslintConfigPrettier,
+
+  {
+    ignores: [
+      'node_modules/',
+      '.next/',
+      'public/',
+      'dist/',
+      'out/',
+      'build/',
+      '.env',
+      '*.config.js',
+      'pacakge-lock.json',
+      'yarn.lock',
+    ],
+  },
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+];
